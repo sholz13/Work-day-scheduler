36ae6df7b0f0b4c8417ffee8097a897085640117
Veev B <veevb@Veevs-Air.home>
2022-01-25 12:40:00 -0500

    js file

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,47 @@
-let plannerEl = $(".container");
-let currentDateEl = $("#currentDay");
-let timeBlockBodyEl = $(".time-block");
-let clearEventsButtonEl = $(".clear-events").on("click", clearAllEvents);
+var plannerEl = $(".container");
+var currentDateEl = $("#currentDay");
+var timeBlockBodyEl = $(".time-block");
+var clearEventsButtonEl = $(".clear-events").on("click", clearAllEvents);
 
-let eventFieldsEl;
-let eventFieldsArray;
+var eventFieldsEl;
+var eventFieldsArray;
 
-let currentHour = moment().hours();
-// Declare variable for the current date and time
-let currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+var currentHour = moment().hours();
+
+var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 
 currentDateEl.text(currentDate);
 setInterval(() => {
     currentDateEl.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 }, 1000);
 
-// Function for building the time blocks
+
 function buildTimeBlocks(hour){
-    // Create and append row for time block
-    let timeRowEl = $("<tr></tr>")
+   
+  var timeRowEl = $("<tr></tr>")
         .attr("data-value", hour)
         .addClass(`d-flex time-row ${hour}`);
     timeBlockBodyEl.append(timeRowEl);
-    // Create and append hour column
-    let hourColumnEl = $("<td></td>")
+   
+    var hourColumnEl = $("<td></td>")
         .attr("scope", "col")
         .addClass("d-flex col-2 col-lg-1 justify-content-center align-items-center")
         .text(moment(`${hour}`, "h").format("hA"));
     timeRowEl.append(hourColumnEl);
-    // Create and append event column
-    let eventColumnEl = $("<td></td>")
+  
+    var eventColumnEl = $("<td></td>")
         .attr("scope", "col")
         .addClass("col-8 col-lg-10");
-    let eventText = $("<textarea></textarea>")
+    var eventText = $("<textarea></textarea>")
         .attr("type", "text")
         .addClass("d-flex form-control col-12 event-input");
     eventColumnEl.append(eventText);
     timeRowEl.append(eventColumnEl);
-    // Create and append save column
-    let saveColumnEl = $("<td></td>")
+    
+    var saveColumnEl = $("<td></td>")
         .attr("scope", "col")
         .addClass("d-flex col-2 col-lg-1 justify-content-center align-items-center");
-    let saveButtonEl = $("<button></button>")
+    var saveButtonEl = $("<button></button>")
         .attr("type", "button")
         .addClass("btn btn-primary far fa-save fa-lg saveBtn")
         .on("click", saveEvent);
@@ -49,7 +49,7 @@ function buildTimeBlocks(hour){
     timeRowEl.append(saveColumnEl);
 }
 
-// Function for highlighting the colors of the row
+
 function highlightEvents(rowNum){
     let tableTimes = $(`.${rowNum}`);
     if (currentHour === rowNum){
@@ -63,7 +63,6 @@ function highlightEvents(rowNum){
     }
 }
 
-// Function to save events that are currently written to localStorage
 function saveEvent(){
     eventFieldsArray = [];
     eventFieldsEl = document.querySelectorAll(".event-input");
@@ -73,10 +72,9 @@ function saveEvent(){
     localStorage.setItem("eventsArray", JSON.stringify(eventFieldsArray));
 }
 
-// Function to retrieve events from localStorage
 function retrieveEvent(){
     eventFieldsEl = document.querySelectorAll(".event-input");
-    // Failsafe for if the eventsArray is undefined in localStorage
+   
     if (localStorage.eventsArray === undefined){
         eventFieldsArray = [];
     } else {
@@ -87,14 +85,14 @@ function retrieveEvent(){
     }
 }
 
-// Function to remove all events from the page
+
 function clearAllEvents(){
     eventFieldsEl = document.querySelectorAll(".event-input");
     let deleteChoice = confirm("This will clear and delete ALL events saved currently! Proceed?");
     if (deleteChoice === false){
         return;
     } else {
-        // Failsafe for if the eventsArray is undefined in localStorage
+
         if (localStorage.eventsArray === undefined){
             eventFieldsArray = [];    
         } else {
@@ -108,11 +106,11 @@ function clearAllEvents(){
     }
 }
 
-// Function to build out the timeblocks, highlight them and associated event text
+
 function initPage(){
-    // Declare a variable for which hour to start the schedule building on
-    let i = 8;
-    // While loop that populates the page with the hour rows
+   
+    let i = 10;
+
     while (i < 19){
         buildTimeBlocks(i);
         highlightEvents(i);
